4b7f33d4439b1c97c8d6850eab52b5427b822ee8
Richard Lovell <richjavalabs@gmail.com>
2015-05-13 10:33:52 +1200

    refactoring

--- application/config/pushbots.php
+++ application/config/pushbots.php
@@ -1,4 +1,4 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-$config["app_id"]="550002751d0ab189448b45a7";
-$config["app_secret"]="3b1cc27235f5395fa8ee97bbf10f43d9";
+$config["app_id"]="";
+$config["app_secret"]="";
