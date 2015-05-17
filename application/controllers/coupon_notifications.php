2e1830d1aa9e186b13e878e24fe8d04cda1aeda0
Richard Lovell <richjavalabs@gmail.com>
2015-05-17 18:15:15 +1200

    refactoring

--- application/controllers/coupon_notifications.php
+++ application/controllers/coupon_notifications.php
@@ -11,9 +11,7 @@ class Coupon_notifications extends Dashboard_Controller {
      * Initiate the controller.
      */
     public function init() {
-
         $this->output->set_template('blank');
-
         $this->load->css('assets/themes/default/css/bootstrap.css');
         $this->load->css('assets/themes/default/css/notifications.css');
         $this->load->model('Coupon');
@@ -32,24 +30,63 @@ class Coupon_notifications extends Dashboard_Controller {
         $this->Coupon->order_by("expiry_date", "desc");
         return $this->Coupon->get_many_by('user_id', $this->uri->segment(3));
     }
+    
+    public function list_items($base_url, $per_page, $uri_segment = 3) {
+	$this->load->helper('url');
+	//$this->load->library('table');
+	//$data = array();$this->get_list_data();
 
-    public function process_items($items, $row) {
-        $items[] = array(
-            $row->title,
-            date('d/m/Y', strtotime($row->expiry_date)),
-            date('d/m/Y', strtotime($row->expiry_date)),
-        );
-        return $items;
+	//pagination (including styling for bootstrap)
+	$this->load->library('pagination');
+	$config['base_url'] = $base_url;
+	$config['per_page'] = $per_page;
+	$config['uri_segment'] = $uri_segment;
+	$config['full_tag_open'] = '<ul class="pagination">';
+	$config['full_tag_close'] = '</ul>';
+	$config['prev_link'] = '&laquo;';
+	$config['prev_tag_open'] = '<li>';
+	$config['prev_tag_close'] = '</li>';
+	$config['next_link'] = '&raquo;';
+	$config['next_tag_open'] = '<li>';
+	$config['next_tag_close'] = '</li>';
+	$config['cur_tag_open'] = '<li class="active"><a href="#">';
+	$config['cur_tag_close'] = '</a></li>';
+	$config['num_tag_open'] = '<li>';
+	$config['num_tag_close'] = '</li>';
+
+	//config variable passed by reference
+	$rows = $this->get_items($config);
+	$config["num_links"] = round($config["total_rows"] / $config["per_page"]);
+
+	$this->pagination->initialize($config);
+
+//	$items = array();
+//	foreach ($rows as $row) {
+//	    $items = $this->process_items($items, $row);
+//	}
+//	$data['items'] = $items;
+//	return $data;
+        $data['items'] = $rows;
+        return $data;
     }
 
-    public function get_list_data() {
-        $tbl_heading = array(
-            '0' => array('data' => 'Title', 'class' => 'col-sm-8'),
-            '1' => array('data' => 'Start Date', 'class' => 'col-sm-2'),
-            '2' => array('data' => 'End Date', 'class' => 'col-sm-2'));
-        return array(
-            'tbl_heading' => $tbl_heading);
-    }
+//    public function process_items($items, $row) {
+//        $items[] = array(
+//            $row->title,
+//            date('d/m/Y', strtotime($row->expiry_date)),
+//            date('d/m/Y', strtotime($row->expiry_date)),
+//        );
+//        return $items;
+//    }
+
+//    public function get_list_data() {
+//        $tbl_heading = array(
+//            '0' => array('data' => 'Title', 'class' => 'col-sm-8'),
+//            '1' => array('data' => 'Start Date', 'class' => 'col-sm-2'),
+//            '2' => array('data' => 'End Date', 'class' => 'col-sm-2'));
+//        return array(
+//            'tbl_heading' => $tbl_heading);
+//    }
 
     public function get_item_name() {
         return $this->item_name;
--- application/views/pages/coupon_notification_list.php
+++ application/views/pages/coupon_notification_list.php
@@ -4,8 +4,27 @@
         </h2>-->
 
     <?php if ($items): ?>
+    <table class="table">
+<thead>
+<tr>
+<th class='col-sm-8'>Title</th><th class='col-sm-2'>Start Date</th><th class='col-sm-2'>End Date</th></tr>
+</thead>
+<tbody>
+<tr>
+<td>ewrwe</td><td>20/05/2015</td><td>20/05/2015</td></tr>
+<tr>
+<td>ewrwe</td><td>20/05/2015</td><td>20/05/2015</td></tr>
+<tr data-id="4">
+<td>asd</td><td>20/05/2015</td><td>20/05/2015</td></tr>
+<tr>
+<td>ewrwe</td><td>20/05/2015</td><td>20/05/2015</td></tr>
+<tr>
+<td>wer</td><td>20/05/2015</td><td>20/05/2015</td></tr>
+</tbody>
+</table>
         <?php
-        include '/../partials/item_list.php';
+        
+        //include '/../partials/item_list.php';
         ?>
     <?php else: ?> 
         <p>There are no coupons</p>
@@ -13,10 +32,16 @@
 </div>
 <script>
     $(document).ready(function ($) {
-        $(".table > tbody > tr").click(function () {
-            var tr = $(this);
-            tr.css("background", "blue");
-            window.location = "/coupon_notifications/detail/" + tr.attr("data");
-        });
+//        $(".table-striped").find('tr[data-target]').on('click', function(){
+//        //or do your operations here instead of on show of modal to populate values to modal.
+//         $('#orderModal').data('orderid',$(this).data('id'));
+//    });
+//        
+//        
+//        $(".table > tbody > tr").click(function () {
+//            var tr = $(this);
+//            tr.css("background", "blue");
+//            window.location = "/coupon_notifications/detail/" + tr.data('id');
+//        });
     });
 </script>
